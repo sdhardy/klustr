567b7e05e663e6d7e816ea88315c983614dacd06
Skyler Hardy <skyler.hardy@gmail.com>
2018-08-16 08:06:07 -0700

    Updated install_ansible.sh for ubuntu 18

--- install_ansible.sh
+++ install_ansible.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Added for ubuntu 18, universe was not enabled after fresh install
+add-apt-repository universe
+
 apt-get update && apt-get upgrade -y
 
 apt-get install -y python-minimal build-essential libssl-dev libffi-dev python-dev python-markupsafe python-pip
